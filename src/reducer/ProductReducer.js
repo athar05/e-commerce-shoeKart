b68aab2dc6c691b28e78eee1ff1361f6bcc9b533
athar05 <atharshakeel96@gmail.com>
2022-04-13 02:34:45 +0530

    Fixes for quantity update while moving products from wishlist to cart

--- src/reducer/ProductReducer.js
+++ src/reducer/ProductReducer.js
@@ -1,3 +1,5 @@
+import CartCard from "../components/cart-card/CartCard";
+
 function ProductReducer(productState, action) {
   switch (action.type) {
     case "ADD_TO_CART":
@@ -21,7 +23,67 @@ function ProductReducer(productState, action) {
         cart: productState.cart.filter((prod) => prod.id !== action.payload.id),
         wishlist: [...productState.wishlist, { ...action.payload }],
       };
+    // case "MOVE_TO_CART":
+    //   return {
+    //     ...productState,
+    //     cart: [...productState.cart, { ...action.payload, qty: 1 }],
+    //     wishlist: productState.wishlist.filter(
+    //       (prod) => prod.id !== action.payload.id
+    //     ),
+    //   };
+
     case "MOVE_TO_CART":
+      // - find out which product you are moving (id, name etc)
+      const prodToMove = productState["wishlist"].find(
+        (prod) => prod.id === action.payload.id
+      );
+
+      let prodToMoveId = prodToMove["id"];
+      console.log(prodToMoveId);
+
+      // - make a copy of the state object (referred to as temp)
+      let { cart: newCart } = productState;
+      console.log(newCart);
+      // - check if product is already added to cart in temp
+      const result = newCart.filter((item) => item.id == prodToMoveId);
+      if (result.length == 0) {
+        // then product does not exist, so add the object to the array
+        newCart = [...productState.cart, { ...action.payload, qty: 1 }];
+      } else {
+        // product exists and qty needs to be changes
+        const newQty = newCart.qty + 1;
+        // in newCart, find the element's index
+        const doesProductMatchID = (element) => element.id == prodToMoveId;
+        const index = newCart.findIndex(doesProductMatchID);
+        console.log(index);
+        console.log(newCart[index]);
+        newCart[index].qty = newCart[index].qty + 1;
+        console.log(newCart[index]);
+        // newCart = [...productState.cart, { ...action.payload, qty: newQty }];
+      }
+
+      // - in wishlist, remove the item from the array in temp
+      let { wishlist: newWishlist } = productState;
+      newWishlist = productState.wishlist.filter(
+        (prod) => prod.id !== action.payload.id
+      );
+
+      // - now the temp value will reflect the wishlist and cart changes
+      // - update state to reflect/replace with temp value
+      return {
+        ...productState,
+        cart: newCart,
+        wishlist: newWishlist,
+      };
+
+      console.log(cart["id"]);
+
+      if (prodToMove["id"].some(productState["cart"])) {
+        console.log("True");
+      } else {
+        console.log("False");
+      }
+
       return {
         ...productState,
         cart: [...productState.cart, { ...action.payload, qty: 1 }],
@@ -29,6 +91,7 @@ function ProductReducer(productState, action) {
           (prod) => prod.id !== action.payload.id
         ),
       };
+
     case "REMOVE_FROM_WISHLIST":
       return {
         ...productState,
